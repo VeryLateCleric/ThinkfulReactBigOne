814287ed6fbbe946f037c7c505ab4c3655c8d88a
Kenneth <kennethyoung.2960@gmail.com>
2024-04-18 13:35:14 -0500

    03 Assess added and 0 of 4 tests passing init

---
+++ 03-StateOverMultipleComponents/React_state_management_State_over_multiple_components_React_18_1/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header({ loggedIn, handleLoggedInClick, handleFontSizeIncrease }) {
+  return (
+    <div>
+    // your code goes here
+    </div>
+  );
+}
+
+export default Header;
