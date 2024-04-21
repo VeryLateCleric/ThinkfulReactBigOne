ad2caf0efef61b42351ceffb7c1fb2bdab4c8849
Kenneth <kennethyoung.2960@gmail.com>
2024-04-21 15:54:02 -0500

    App.js written to complete TODOs

--- 07-React-State-Management-Assess/React_state_management_Assessment_React_18_1/src/App.js
+++ 07-React-State-Management-Assess/React_state_management_Assessment_React_18_1/src/App.js
@@ -5,12 +5,24 @@ import PostList from "./PostList";
 
 function App() {
   const [posts, setPosts] = useState([]);
-  // TODO: Add the ability for the <PostCreate /> component to create new posts.
-  // TODO: Add the ability for the <PostList /> component to delete an existing post.
+
+  const createPost = (type, content) => {
+    const newPost = { type, content };
+    setPosts([...posts, newPost]);
+  };
+  
+  const deletePost = (index) => {
+    const updatedPosts = [...posts];
+    updatedPosts.splice(index, 1);
+    setPosts(updatedPosts);
+  };
+
+  // TODO: CHECK DONE Add the ability for the <PostCreate /> component to create new posts.
+  // TODO: CHECK DONE Add the ability for the <PostList /> component to delete an existing post.
   return (
     <div className="App">
-      <PostCreate />
-      <PostList />
+      <PostCreate createPost={createPost} />
+      <PostList posts={posts} deletePost={deletePost} />
     </div>
   );
 }
